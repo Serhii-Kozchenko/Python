4ec888c0d2b72313a408e860fb7148d476f5a037
Serhii-Kozchenko <serhii.k4work@gmail.com>
2024-01-05 19:24:34 +0200

    8

--- main.py
+++ main.py
@@ -461,9 +461,7 @@
 
 from datetime import datetime
 
-# current_time = datetime.now()
 
-# print(current_time)
 
 holy_evening = datetime(year=2024, month=1, day=6, hour=16)
 current_time = datetime.now()
